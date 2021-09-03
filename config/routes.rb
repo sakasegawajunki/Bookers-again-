60a65a90c3f9b54b8d2107268c7e1c803504316d
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-03 07:45:13 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :users
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
