2304b99a6ed83fc0a4a6afb1acba6d63690aef47
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-04 05:54:45 +0000

    ファイルの設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  resources :users, only:[:show, :index, :edit]
-  resources :books, only:[:new, :creat, :index, :show, :destroy]
-  devise_for :users
+  # get 'homes/top'
   root to: 'homes#top'
+  get 'home/about'
+  resources :users, only:[:show, :index, :edit]
+  resources :books, only:[:create, :index, :show, :destroy, :edit]
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
