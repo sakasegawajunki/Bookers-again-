6f0ec6c053d15a26802109f057e36d1b9f8f5b4b
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-06 03:40:28 +0000

    User info　作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
   def new
-    
+
   end
 
   def creat
   end
 
   def index
+    @user = current_user
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attachment :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
   has_many :books
+  
 end
