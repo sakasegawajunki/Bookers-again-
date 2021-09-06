65b7b385847313db31b864356f07948a0c28dd02
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-06 13:38:18 +0000

    投稿機能と閲覧機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,21 @@
 class BooksController < ApplicationController
-  def new
 
-  end
-
-  def creat
-  end
 
   def index
     @user = current_user
+    @book = Book.new
+    @books =  Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id),notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def show
@@ -15,4 +23,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
