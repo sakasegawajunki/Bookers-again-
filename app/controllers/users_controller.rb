960251c21ec934b73f4b6b85f7de1a193ec88fc3
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-07 02:47:15 +0000

    userのindexページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
+    @user = current_user
+
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:image, :introduction)
   end
 end
