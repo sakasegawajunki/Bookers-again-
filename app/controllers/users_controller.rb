0cf05d04bb43dbb35fb6ae67574f2a81a48de563
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-07 03:50:43 +0000

    クローン後コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+  
   def show
-    
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def index
+    @users = User.all
+    @book = Book.new
   end
 
+
   def edit
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
   end
 end
