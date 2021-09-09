f669ec8b078e6acaba124ed01e49b5c13e8250a0
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-09 02:53:49 +0000

    bookのeditページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,8 +22,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
   end
-  
+
   def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    if @user != current_user
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+
   end
 
   def destroy
